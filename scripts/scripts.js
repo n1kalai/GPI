0d71c04589bd74265ba03e8abe641283f747eb2d
Nikoloz <47790698+n1kalai@users.noreply.github.com>
2019-10-22 22:04:16 +0400

    Getting Started

---
+++ scripts/scripts.js
@@ -0,0 +1,30 @@
+let $mainHeader = document.querySelector('.mainHeader');
+let $ul = document.querySelector('.mainHeader ul');
+console.log($ul)
+//console.log($gia[0])
+window.onscroll = function (){
+     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)  {
+    $mainHeader.style.position = 'fixed';
+    $mainHeader.style.top = '0px';
+    $mainHeader.style.background = 'white';
+    $mainHeader.style.animation = 'forHeader .2s ease-in-out forwards';
+         $mainHeader.style.backgroundImage = 'url(images/geo.png)';
+         $mainHeader.style.backgroundSize = '220px 80px';
+         $mainHeader.style.backgroundRepeat = 'no-repeat';
+         $mainHeader.style.backgroundPosition = 'center';
+    $ul.style.color = 'rgba(8, 63, 110, 0.51)';
+    
+   
+    
+} else {
+    $mainHeader.style.position = null;
+    $mainHeader.style.top = null;
+    $mainHeader.style.background = null;
+    $mainHeader.style.width = null;
+    $mainHeader.style.left = null;
+    $ul.style.color = null;
+     $mainHeader.style.animation = null;
+//    $mainHeader.style.animation = null
+    
+}
+}
