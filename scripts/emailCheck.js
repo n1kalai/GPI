748874b889c2d2ac6aae48cc3952dbe461f8bf1c
Nikoloz <47790698+n1kalai@users.noreply.github.com>
2019-11-14 12:31:43 +0400

    registration changing

--- scripts/emailCheck.js
+++ scripts/emailCheck.js
@@ -1,14 +1,26 @@
 let button = document.querySelector('button');
-let input = document.querySelector('.eMail');
-button.addEventListener('click', mailCHeck);
+// let input = document.querySelector('.eMail');
+// button.addEventListener('click', mailCHeck);
 
-function mailCHeck(e){
+// function mailCHeck(e){
 
     
-    if (input.value.indexOf('@') === -1 || input.value.indexOf('.') === -1){
-        alert('შეიყვანეთ სწორი ელ-ფოსტა');
-        e.preventDefault();
+//     if (input.value.indexOf('@') === -1 || input.value.indexOf('.') === -1){
+//         alert('შეიყვანეთ სწორი ელ-ფოსტა');
+//         e.preventDefault();
+//     }
+
+// }
+let eMail = document.querySelector('.eMail');
+
+eMail.addEventListener('blur',mailCheck);
+function mailCheck(e){
+    let text = document.querySelector('.forEmail');
+    if (eMail.value.indexOf('@') === -1 ||eMail.value.indexOf('.') === -1 || eMail.value.indexOf('.') === eMail.value.length - 2  || eMail.value.indexOf('.') === eMail.value.length - 1){
+        text.style.opacity = 1;
+        text.style.color = 'red';
+    } else {
+        text.style.color = 'green';
+        text.style.opacity = 0;
     }
-
 }
-
